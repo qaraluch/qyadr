3d19092324d328936d8d3e99d48b9688f9b76f33
qaraluch <prezenciq@gmail.com>
2018-08-16 11:46:06 +0200

    --- stash prompt dev

--- zsh/.zsh-prompt.zsh
+++ zsh/.zsh-prompt.zsh
@@ -1,1 +1,9 @@
-echo "This si zshrc_test"
+# My 1
+# PS1="[%# %n@%m ] %~ λ "
+PS1="[%#]%(1j.[%j].)%\[ %n@%m ] %{%F{cyan}%~%} %(?.%F{green}λ.%F{red}λ)%f "
+
+# helper function to add empty line before prompt redrawc
+precmd() { print "" }
+
+# Default oh-my-zsh:
+# PS1="%n@%m:%~%# "
