27bf4a35a510b0e86165b6aaf92587f7dc6f0e72
qaraluch <prezenciq@gmail.com>
2018-08-23 12:52:08 +0200

    Add key binding to accept autosuggestion

--- zsh/.zsh-options.zsh
+++ zsh/.zsh-options.zsh
@@ -37,6 +37,7 @@ autoload -U edit-command-line         # edit command in editro (in vim)
 zle -N edit-command-line
 
 # Bindkeys
+bindkey '^ ' autosuggest-accept                   # ctrl+space (or right arrow)
 bindkey '\C-x\C-e' edit-command-line              # ctrl-x ctrl-e - edit command in editro (in vim)
 bindkey "\C-x\C-l" zle-insert-last-typed-word     # ctrl-x ctrl-l - insert last typed word, see: .functions/zsh-terminal.zsh
 bindkey '\C-x\C-b' zle-slash-backward-kill-word   # ctrl-x ctrl-b - (like back) slash cut paths, see: .functions/zsh-terminal.zsh 
