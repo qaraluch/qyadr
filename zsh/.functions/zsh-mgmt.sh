8e84f9323bcc4f47b123627a60c2253218a56c3e
qaraluch <prezenciq@gmail.com>
2018-12-29 12:59:38 +0100

    Refactored zsh-mgmt.sh functions

--- zsh/.functions/zsh-mgmt.sh
+++ zsh/.functions/zsh-mgmt.sh
@@ -4,17 +4,17 @@
 
 zsh-reload() {
   source ~/.zshrc
-  echoIt "Reloaded ${C_Y}ZSH${C_E} shell config." "${I_T}"
+  _echoIt "$_QDel" "Reloaded ${_Qcy}ZSH${_Qce} shell config." "${_Qit}"
 }
 
 zsh-measure-loading-times() {
-  local OUTPUTFILE="${HOME}/.zsh-time.txt"
-  local TIMESTAMP=$(getTimeStampHuman)
-  echoIt "About to measure ZSH loading time..."
-  echo "\n ---------- ${TIMESTAMP} --------------------------------------------" >> $OUTPUTFILE
-  for i in {1..3}; do (zsh-run-loading-time >/dev/null ) 2>> $OUTPUTFILE 2>&1; done
-  echo "  --> See historical data in file: $OUTPUTFILE"
-  echoDone
+  local outputPath="${HOME}/.zsh-profiling.log"
+  local timeStamp=$(_getTimeStampHuman)
+  _echoIt "$_QDel" "About to measure ZSH loading time..."
+  echo "\n ---------- ${timeStamp} --------------------------------------------" >> $outputPath
+  for i in {1..3}; do (zsh-run-loading-time >/dev/null ) 2>> $outputPath 2>&1; done
+  _echoIt "$_QDel" "  --> See historical data in file: $outputPath"
+  _echoDone
 }
 
 zsh-run-loading-time() {
