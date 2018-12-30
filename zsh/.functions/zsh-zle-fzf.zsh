50f3ef84917e505e4bafe478bb38919b1e2139e7
qaraluch <prezenciq@gmail.com>
2018-12-30 10:16:38 +0100

    Refactored zsh functions

--- zsh/.functions/zsh-zle-fzf.zsh
+++ zsh/.functions/zsh-zle-fzf.zsh
@@ -1,58 +1,57 @@
 # ZSH terminal helpers and utils functions with help of fzf
 # dependencies:
-# fzf
-# git
+#  - fzf
+#  - git
 
 fzf-choose-git-msg() {
  echo "\"$( git-get-logs | fzf --layout=reverse --border | cut -d " " -f2-)\""                                    # see: git/.functions/git.sh
 }
 
 zle-fzf-git-get-commit-msg() {
-  if git-is-in-repo ; then
-    _zle-init-widget
+  if git-is-in-repo ; then	                               # see: git/.functions/git.sh
+    _zle-init-widget                                       # see: zsh/.functions/zsh-zle.zsh
     fzf-choose-git-msg | \
     _zle-insert
   else
-    zle -M "    ... Sorry. Not in git repo!"           
+    zle -M "    ... Sorry. Not in git repo!"
   fi
 }
 
 zle -N gmsg zle-fzf-git-get-commit-msg                      # usage: in vi mode: hit <esc>, :, and type gmsg
-                                                            # or use alias "zle -A zle-fzf-git-get-commit-msg gmsg"
 
 fzf-choose-git-hash() {                                     # see: git/.functions/git.sh
   echo "$( git-get-logs | \
     fzf --height 60% --layout=reverse --border | \
     cut -d " " -f1
-  )"                                                
+  )"
 }
 
 zle-fzf-git-get-hash () {
   if git-is-in-repo ; then
-    _zle-init-widget
+    _zle-init-widget                                        # see: zsh/.functions/zsh-zle.zsh
     fzf-choose-git-hash | \
     _zle-insert
   else
-    zle -M "    ... Sorry. Not in git repo!"           
+    zle -M "    ... Sorry. Not in git repo!"
   fi
 }
 
-zle -N ghash zle-fzf-git-get-hash 
+zle -N ghash zle-fzf-git-get-hash
 
 fzf-choose-git-status-item() {                              # see: git/.functions/git.sh
   echo "$( git-get-status-items | \
     fzf --height 40% --layout=reverse --border -m | \
-    awk '{print $2}' 
-  )"              
+    awk '{print $2}'
+  )"
 }
 
 zle-fzf-git-get-status-item() {
   if git-is-in-repo ; then
-    _zle-init-widget
+    _zle-init-widget                                        # see: zsh/.functions/zsh-zle.zsh
     fzf-choose-git-status-item | \
     _zle-insert
   else
-    zle -M "    ... Sorry. Not in git repo!"           
+    zle -M "    ... Sorry. Not in git repo!"
   fi
 }
 
--- zsh/.functions/zsh-zle.zsh
+++ zsh/.functions/zsh-zle.zsh
@@ -3,7 +3,7 @@
 # ???
 
 # zle utils
-_zle-init-widget() {                # when init widget, save what is typed in command line 
+_zle-init-widget() {                # when init widget, save what is typed in command line
   if [[ -n $1 ]]; then
     BUFFER="$1 "
     CURSOR=$#BUFFER
@@ -39,27 +39,30 @@ _zle-clear() {
 }
 
 # From zsh-pony
-zle-insert-last-typed-word() { 
+zle-insert-last-typed-word() {
   zle insert-last-word -- 0 -1
 };
+
 zle -N zle-insert-last-typed-word;
 
 # From zsh-pony
 zle-slash-backward-kill-word() {
-  local WORDCHARS="${WORDCHARS:s@/@}"
+  local wordChars="${wordChars:s@/@}"
   zle backward-kill-word
 }
+
 zle -N zle-slash-backward-kill-word
 
 # From .grml.org
 # Useful to add options.
 zle-jump-after-first-word() {
-  local WORDS
-  WORDS=(${(z)BUFFER})
-  if (( ${#WORDS} <= 1 )) ; then
+  local words
+  words=(${(z)BUFFER})
+  if (( ${#words} <= 1 )) ; then
       CURSOR=${#BUFFER}
   else
-      CURSOR=${#${WORDS[1]}}
+      CURSOR=${#${words[1]}}
   fi
 }
+
 zle -N zle-jump-after-first-word
