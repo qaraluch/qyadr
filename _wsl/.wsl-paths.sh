ddac96ed40bff7a8519586d83f87a2d33238798e
qaraluch <prezenciq@gmail.com>
2019-01-10 07:42:11 +0100

    [wsl] Add qnb and f-mgmt

--- _wsl/.wsl-paths.sh
+++ _wsl/.wsl-paths.sh
@@ -3,7 +3,9 @@
 # Main
 export WSL_C="/mnt/c"
 export WSL_G="/mnt/g"
+export WSL_H="/mnt/h"
 export WSL_WIN_HOME="${WSL_C}/Users/${QYADR_SEC_EP_WIN_USER}"
 
-# ...
-export WSL_STH="${WSL_C}/sth"
+# For source
+export WSL_QNB="${WSL_G}/qnb"
+export WSL_F_MGMT="${WSL_H}/f-mgmt"
