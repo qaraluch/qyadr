5d5d732784803a13df912f689d27d8c446f08220
qaraluch <prezenciq@gmail.com>
2019-02-10 11:22:46 +0100

    Add ytd-mgmt to WSL rc

--- _wsl/.wsl-paths.sh
+++ _wsl/.wsl-paths.sh
@@ -11,6 +11,7 @@ export WSL_QNB="${WSL_G}/qnb"
 export WSL_QB="${WSL_G}/qb"
 export WSL_ATV="${WSL_G}/atv"
 export WSL_F_MGMT="${WSL_H}/f-mgmt"
+export WSL_YTD_MGMT="${WSL_H}/ytd-mgmt"
 
 # Dirs
 export WSL_WIN_CONFIG="${WSL_G}/config"
