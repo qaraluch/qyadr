d55dbaab26fec6c5dcf091d0691cb94cd0c9bfa9
qaraluch <prezenciq@gmail.com>
2019-03-29 10:25:36 +0100

    Change f-mgmt dir to v2 in .zshrc-wsl

--- _wsl/.wsl-paths.sh
+++ _wsl/.wsl-paths.sh
@@ -8,7 +8,7 @@ export WSL_WIN_HOME="${WSL_C}/Users/${QYADR_SEC_EP_WIN_USER}"
 
 # For source
 export WSL_QNB="${WSL_G}/qnb"
-export WSL_F_MGMT="${WSL_H}/f-mgmt"
+export WSL_F_MGMT="${WSL_H}/f-mgmt2"
 export WSL_YTD_MGMT="${WSL_H}/ytd-mgmt"
 
 # Dirs
