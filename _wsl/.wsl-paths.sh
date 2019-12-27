d936ea2c6eb5d8a55f60e2f0421892eeb3b4537c
qaraluch <prezenciq@gmail.com>
2019-12-27 14:05:46 +0100

    Add wsl paths for tmux scripts

--- _wsl/.wsl-paths.sh
+++ _wsl/.wsl-paths.sh
@@ -16,3 +16,11 @@ export WSL_TILJS="${WSL_H}/dev/tiljs"
 export WSL_WIN_CONFIG="${WSL_G}/config"
 export WSL_WIN_DROPBOX="${WSL_G}/Dropbox"
 export WSL_WIN_DEV="${WSL_H}/dev"
+
+#TODO: cheange names and targets
+export qyadrLocalPath="$HOME/.qyadr"
+export bashdevPath="$WSL_QNNB/shell/bashdev"
+export qnbPath="/mnt/g/qnb"
+export qyadrPath="/mnt/g/config/qyadr"
+export archBootstrapPath="/mnt/h/dev/tool/arch-bootstrap"
+
