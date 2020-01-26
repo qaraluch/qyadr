cdfb844be605774032c58b99fc42bf7b4adc6e5e
qaraluch <prezenciq@gmail.com>
2020-01-26 10:27:03 +0100

    Change wsl and arch patch file to vars file name

---
+++ _arch/.vars-arch.sh
@@ -0,0 +1,6 @@
+# Arch Vars
+
+export qnnbPath="${HOME}/qnnb"
+export qyadr_localPath="${HOME}/.qyadr"
+export bashdevPath="${qnnbPath}/shell/bashdev"
+export arch_bootstrapPath="${HOME}/dev/arch-bootstrap"
