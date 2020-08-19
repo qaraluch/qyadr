c7a88c3c84efbe9671acbc8f32823acdd32b4ee2
qaraluch <prezenciq@gmail.com>
2020-08-19 08:02:52 +0200

    Update snippets - all of them

--- zsh/.zsh-bindkeys.zsh
+++ zsh/.zsh-bindkeys.zsh
@@ -16,7 +16,7 @@ bindkey '\C-e'    fzf-cd-widget                   # rebinding from alt-c in orde
 bindkey '\C-x\C-e' edit-command-line              # ctrl-x ctrl-e - edit command in editor (in vim)
 bindkey '\C-x\C-l' zle-insert-last-typed-word     # ctrl-x ctrl-l - insert last typed word, see: .functions/zsh-zle.zsh
 bindkey '\C-x\C-f' zle-jump-after-first-word      # ctrl-x ctrl-f - jump after first word to add options, see: .functions/zsh-zle.zsh
-bindkey '\C-x\C-d' zle-insert-datestamp           # ctrl-x ctrl-f - insert timestamp
+bindkey '\C-x\C-d' zle-insert-datestamp           # ctrl-x ctrl-d - insert timestamp
 
 # Extra functionality from oh-my-zsh
 # start typing + [Up/Down-Arrow] - fuzzy find history forward
